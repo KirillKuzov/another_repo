26144745e9f83f3fb908ac2ac483641dbbfdcc2c
kirill <kirill@DESKTOP-2IAM7BE>
2024-01-10 20:34:49 +0300

    add file

--- str_func.py
+++ str_func.py
@@ -4,3 +4,11 @@ def function():
     user_input = input(str())
     return user_input.upper()
 
+
+def new_func():
+    """
+    Делает заглавными только первые буквы каждого слова в строке
+    :return:
+    """
+    user_input = input(str())
+    print(user_input.title())
